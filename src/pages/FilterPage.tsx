d5202b9d46960ba9ba663f26945632fd26e6f57d
faridulhaque <faridulhaquemurshed@gmail.com>
2023-01-26 11:09:45 +0600

    filter ui ready

--- src/pages/FilterPage.tsx
+++ src/pages/FilterPage.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { Box } from "@mui/material";
+import React from "react";
+import SelectLaunchStatus from "../components/filter/SelectLaunchStatus";
+import SelectTime from "../components/filter/SelectTime";
+import UpcomingCheck from "../components/filter/UpcomingCheck";
+import Navbar from "../components/shared/Navbar";
 
 const FilterPage = () => {
-    return (
-        <>
-            <h2>Oh my filter</h2>
-        </>
-    );
+  return (
+    <>
+      <Navbar></Navbar>
+      <Box width="96%" marginLeft="2%" marginRight="2%" marginTop="100px">
+        <Box width="100%" display="flex">
+          <SelectLaunchStatus></SelectLaunchStatus>
+          <SelectTime></SelectTime>
+        </Box>
+        <UpcomingCheck></UpcomingCheck>
+      </Box>
+    </>
+  );
 };
 
 export default FilterPage;
